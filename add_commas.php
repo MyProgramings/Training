dbbe09e92e5ade59b95eeca4fe32b611e332bd11
MyProgramings <reader1999a@gmail.com>
2024-10-09 19:53:06 +0300

    done add commes

--- add_commas.php
+++ add_commas.php
@@ -9,10 +9,39 @@
 
 <body>
     <?php
-    print('323');
-    print('3243');
-    print('32443434343');
-    print('35983767267676778989');
+    function add_commas_and_underscore($number)
+    {
+        $array = str_split($number);
+        $new_array = [];
+        $three_num = 0;
+        if (count($array) <= 3)
+            return $number;
+        else {
+            for ($i = count($array) - 1; $i >= 0; $i--) {
+                $three_num++;
+                if ($three_num % 3 == 0) {
+                    array_push($new_array, $array[$i]);
+                    if($three_num < 6)
+                        array_push($new_array, "_");
+                    else
+                        array_push($new_array, ",");
+                }
+                else
+                    array_push($new_array, $array[$i]);
+            }
+        }
+        $new_array = array_reverse($new_array);
+        return implode($new_array);
+    }
+    echo ("<pre>");
+    print_r(add_commas_and_underscore(120)); # 120
+    echo ("<pre>");
+    print_r(add_commas_and_underscore(1530)); # 1_530 
+    echo ("<pre>");
+    print_r(add_commas_and_underscore(12069057014032)); # 12,069,057,014_032
+    echo ("<pre>");
+    print_r(add_commas_and_underscore(1206905701768840372)); # 12,069,057,014_032
+    echo ("<pre>");
     ?>
 </body>
 
